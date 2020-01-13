f7a3e492cac9c4118c2f3c67ca04ea13ef325aa8
daizpuru <daizpuru@gmail.com>
2020-01-13 13:32:24 +0100

    Commit inicial

---
+++ dockerfile/mysql/sql-scripts/CreateTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE alumnos (
+  nombre varchar(25),
+  apellido1 varchar(25),
+  apellido2 varchar(25),
+  f_nacimiento date,
+  email  varchar(50)
+);
---
+++ dockerfile/mysql/sql-scripts/InsertData.sql
@@ -0,0 +1,7 @@
+INSERT INTO alumnos (nombre, apellido1, apellido2, f_nacimiento, email) 
+VALUES ('Daniel', 'Perez', 'Gomez', STR_TO_DATE('12-05-2000', '%d-%m-%Y'), 'dperez@uva.es');
+INSERT INTO alumnos (nombre, apellido1, apellido2, f_nacimiento, email)
+VALUES ('Eva', 'Arranz', 'Salas', STR_TO_DATE('11-11-2001', '%d-%m-%Y'), 'evaarranz@eva.es');
+INSERT INTO alumnos (nombre, apellido1, apellido2, f_nacimiento, email)
+VALUES ('Raul', 'Lopez', 'Gomez', STR_TO_DATE('13-10-2000', '%d-%m-%Y'), 'rlopez@uva.es');
+
