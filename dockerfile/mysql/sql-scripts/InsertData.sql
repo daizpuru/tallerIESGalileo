86020bd5b7e8d089861ded55eb189ad18fdb1527
daizpuru <daizpuru@gmail.com>
2020-01-13 17:09:41 +0100

    Update InsertData.sql

--- dockerfile/mysql/sql-scripts/InsertData.sql
+++ dockerfile/mysql/sql-scripts/InsertData.sql
@@ -1,7 +1,7 @@
 INSERT INTO alumnos (nombre, apellido1, apellido2, f_nacimiento, email) 
-VALUES ('Daniel', 'Perez', 'Gomez', STR_TO_DATE('12-05-2000', '%d-%m-%Y'), 'dperez@uva.es');
+VALUES ('Daniel', 'Perez', 'Gomez', STR_TO_DATE('12-05-2000', '%d-%m-%Y'), 'dperez@educa.jcyl.es');
 INSERT INTO alumnos (nombre, apellido1, apellido2, f_nacimiento, email)
-VALUES ('Eva', 'Arranz', 'Salas', STR_TO_DATE('11-11-2001', '%d-%m-%Y'), 'evaarranz@eva.es');
+VALUES ('Eva', 'Arranz', 'Salas', STR_TO_DATE('11-11-2001', '%d-%m-%Y'), 'evaarranz@educa.jcyl.es');
 INSERT INTO alumnos (nombre, apellido1, apellido2, f_nacimiento, email)
-VALUES ('Raul', 'Lopez', 'Gomez', STR_TO_DATE('13-10-2000', '%d-%m-%Y'), 'rlopez@uva.es');
+VALUES ('Raul', 'Lopez', 'Gomez', STR_TO_DATE('13-10-2000', '%d-%m-%Y'), 'rlopez@educa.jcyl.es');
 
